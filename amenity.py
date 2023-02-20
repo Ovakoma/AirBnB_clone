c3104ec2e65c95d2b3e4024d26452375a0f6a511
Ovakoma <ideleovercomer18@gmail.com>
2023-02-20 23:40:40 +0000

    removed duplicate files

--- amenity.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""file contains Amenity class"""
-
-from models.base_model import BaseModel
-
-
-class Amenity(BaseModel):
-    """This class also inherits from the BaseModel class"""
-    name = ""
--- state.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""file contains State class"""
-
-from models.base_model import BaseModel
-
-
-class State(BaseModel):
-    """This class inherits from BaseModel"""
-    name = ""
